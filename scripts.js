2584f91fe13f25a1865b1e2553d93ddc472ba613
Jeferson Paz <80724826+jeferson-paz@users.noreply.github.com>
2023-11-13 18:25:22 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // ScrollSpy
+window.addEventListener('DOMContentLoaded', event => {
+    // Código que precisa esperar pelo DOM estar pronto vai aqui
+
+    // Lógica para inicializar o ScrollSpy (assumindo que você já importou Bootstrap)
     const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
         new bootstrap.ScrollSpy(document.body, {
@@ -8,10 +10,10 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    // Fechar menu responsivo ao clicar em um item de navegação
+    // Lógica para fechar o menu responsivo ao clicar em um item de navegação
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = Array.from(document.querySelectorAll('#navbarResponsive .nav-link'));
-    responsiveNavItems.forEach(function (responsiveNavItem) {
+    responsiveNavItems.map(function (responsiveNavItem) {
         responsiveNavItem.addEventListener('click', () => {
             if (window.getComputedStyle(navbarToggler).display !== 'none') {
                 navbarToggler.click();
@@ -19,15 +21,21 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    // Botão "Voltar ao Topo"
-    const botaoVoltarAoTopo = document.getElementById("voltar-ao-topo");
+    // Lógica para o botão de "voltar ao topo"
+    var botaoVoltarAoTopo = document.getElementById("voltar-ao-topo");
 
     window.addEventListener("scroll", function() {
-        // Mostra ou oculta o botão com base no deslocamento da página
-        botaoVoltarAoTopo.style.display = window.pageYOffset > 200 ? "block" : "none";
+        if (window.pageYOffset > 200) {
+            // Mostra o botão quando o usuário rolar 200 pixels para baixo
+            botaoVoltarAoTopo.style.display = "block";
+        } else {
+            // Oculta o botão quando o usuário estiver no topo da página
+            botaoVoltarAoTopo.style.display = "none";
+        }
     });
 
     botaoVoltarAoTopo.addEventListener("click", function() {
-        // Rola suavemente até o topo da página
+        // Faz a animação de rolagem suave até o topo da página
         window.scrollTo({ top: 0, behavior: "smooth" });
     });
+});
