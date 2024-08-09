951be35097cfae6d05da4adfee03b53a767d7adb
Jeferson Paz <80724826+jeferson-paz@users.noreply.github.com>
2024-08-09 11:27:22 -0300

    Upadate

--- scripts.js
+++ scripts.js
@@ -1,23 +1,26 @@
 // Suavizar rolagem
 $(document).ready(function() {
+    // Seleciona todos os links com a classe 'js-scroll-trigger' que possuem um atributo href contendo '#', exceto os que têm href igual a '#'
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function(e) {
-        e.preventDefault();
+        e.preventDefault(); // Previne o comportamento padrão do link
+
+        // Anima a rolagem para o elemento alvo
         $('html, body').animate({
             scrollTop: $($(this).attr('href')).offset().top
-        }, 1000); // Ajuste o tempo de rolagem conforme necessário
+        }, 1000); // Tempo de rolagem de 1000 milissegundos (1 segundo)
     });
 });
 
 // Mostrar o botão de voltar ao topo quando o usuário rolar para baixo
 $(window).scroll(function() {
-    if ($(this).scrollTop() > 10) {
-        $('#voltar-ao-topo').fadeIn();
+    if ($(this).scrollTop() > 10) { // Se o usuário rolou para baixo mais de 10 pixels
+        $('#voltar-ao-topo').fadeIn(); // Mostra o botão
     } else {
-        $('#voltar-ao-topo').fadeOut();
+        $('#voltar-ao-topo').fadeOut(); // Oculta o botão
     }
 });
 
 // Quando o botão for clicado, rolar para o topo da página
 $('#voltar-ao-topo').click(function() {
-    $('html, body').animate({scrollTop: 0}, 1000); // Ajuste o tempo de rolagem conforme necessário
+    $('html, body').animate({scrollTop: 0}, 1000); // Anima a rolagem de volta ao topo com um tempo de 1000 milissegundos (1 segundo)
 });
